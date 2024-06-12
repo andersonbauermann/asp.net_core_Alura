903d646aff7c8c9924b90cb4285e1aaa40dcaf1e
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-06-12 14:40:27 -0300

    create class Movie and FilmController

---
+++ filmsAPI/Controllers/FilmController.cs
@@ -0,0 +1,17 @@
+﻿using filmsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmsAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmController : ControllerBase
+{
+    private static List<Movie> movies = new();
+
+    [HttpPost]
+    public void AddFilm([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+    }
+}
